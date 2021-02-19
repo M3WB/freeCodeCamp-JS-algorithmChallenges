ae6313b9831468303672dba2c7ad7b7aebf3ea3b
Mark Brown <mark.m3wb@gmail.com>
2021-02-19 14:57:22 -0600

    Added Comment

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,6 @@
+// Basic Algorithms Challenge #2
+// Write an algorithm that takes a string input by the user and outputs the reverse of that string
+
 function reverseString(str) {
     let strArr = [];
     for (let i = 0; i < str.length; i++){
