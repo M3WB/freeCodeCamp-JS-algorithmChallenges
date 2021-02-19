f71c8125f988075c339c0c7cbee384262a749369
Mark Brown <mark.m3wb@gmail.com>
2021-02-19 14:49:43 -0600

    Challenge 1 solution

---
+++ celsius-to-fahrenheit.js
@@ -0,0 +1,9 @@
+// Basic Algorithms Challenge #1
+// Function to convert a temperature in celsius as a number that outputs temperature in fahrenheit as a number
+
+function convertToF(celsius) {
+    let fahrenheit = celsius*(9/5)+32;
+    return fahrenheit;
+  }
+  
+  convertToF(30);
